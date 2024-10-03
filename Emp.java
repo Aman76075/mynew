f059029d761e20806a6c6dc1d5adfd53c2d42d67
Aman Pandey <amanpandey114@gmail.com>
2024-10-03 15:25:33 +0530

    commit-2

--- Emp.java
+++ Emp.java
@@ -1,8 +1,13 @@
 public class emp {
     emp(){
-        
+        System.out.println("Hello Default Constructor Called.");
+    }
+    public void disp(){
+        System.out.println("Hellp this is method called.");
     }
     public static void main(String[] args){
+        emp e=new emp();
+        e.disp();
         System.out.println("Hello World");
     }
 }
